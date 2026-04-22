cc7465fc8eb12b5bd719d8da9aed6fc25e10791c
ArtursDavidenko <artursdavidenko@gmail.com>
2026-04-22 10:25:40 +0300

    infrastructure update: put backend in docker and set up env

--- RPG API/IdleRpgApi/Program.cs
+++ RPG API/IdleRpgApi/Program.cs
@@ -101,6 +101,12 @@ namespace IdleRpgApi
                 app.UseSwaggerUI();
             }
 
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.Migrate();
+            }
+
             app.UseCors("AllowAngularApp");
             app.UseHttpsRedirection();
 
